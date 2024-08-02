a20a7b6be7465351cc5fc767d204267aca1c4422
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-08-03 03:21:32 +0530

    refactor: made the filter widget fixed

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -6,7 +6,9 @@ export const ItemCard = ({ item }) => {
   return (
     <div className={cardStyles.card}>
       <img className={cardStyles.image} src={image} alt={title} />
-      <p className={cardStyles.title}>{title}</p>
+      <p className={cardStyles.title}>
+        {title.length > 40 ? title.slice(0, 40) + "..." : title}
+      </p>
       <p className={cardStyles.price}>₹ {price}</p>
       <button className={cardStyles.btn}>Add To Cart</button>
     </div>
